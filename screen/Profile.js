58f0aadfe273f3fac7c1e0ff48e69716befe3aed
aumtitikarn <aumzaa321@gmail.com>
2023-10-09 16:18:03 +0700

    .

--- screen/Profile.js
+++ screen/Profile.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Text, TouchableOpacity, View, StyleSheet, Image, Button, SafeAreaView, ScrollView } from 'react-native';
-import Post from '../components/Post';
+import { FIREBASE_AUTH } from 'firebase/app';
 import { Avatar } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
-import { FIREBASE_AUTH } from '../firestore';
+import  Createpost  from './createpost';
 
   const Profile = () => {
   const navigation = useNavigation();
@@ -11,7 +11,7 @@ import { FIREBASE_AUTH } from '../firestore';
 
   const handleLogout = async () => {
     try {
-      await auth.signOut();
+      await auth().signOut();
       navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
     } catch (error) {
       console.error(error);

ebe1dfda908f150eb2e7d6a7928b0b986fa94dbc
Phornthi <fernphornthiwa906@gmail.com>
2023-10-28 21:32:39 +0700

    .

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text,Card,ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 
-const Search = () => {
+const Searchba = ({ onSearchResults }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredData, setFilteredData] = useState([]);
 
   const loadFirestoreData = async () => {
     const db = getFirestore();
-    const postShopCollection = collection(db, 'allpostHome');
-    const q = query(postShopCollection, where('faculty', '==', searchQuery));
+    const postHomeCollection = collection(db, 'allpostHome');
+    const q = query(postHomeCollection, where('faculty', '==', searchQuery));
 
     const querySnapshot = await getDocs(q);
-    const postShopData = [];
+    const postHomeData = [];
 
     querySnapshot.forEach((doc) => {
-      postShopData.push({ id: doc.id, ...doc.data() });
+      postHomeData.push({ id: doc.id, ...doc.data() });
     });
 
-    setFilteredData(postShopData);
+    setFilteredData(postHomeData);
+    onSearchResults(postHomeData);
   };
 
   const handleSearch = (query) => {
@@ -27,6 +28,28 @@ const Search = () => {
     loadFirestoreData();
   };
 
+  const renderItem = ({ item }) => (
+    <TouchableOpacity key={item.id}>
+      <Text style={{ left: 20 }}>{item.faculty}</Text>
+    </TouchableOpacity>
+  );
+  const loalData = async () => {
+    const db = getFirestore();
+    const postHomeCollection = collection(db, 'allpostHome');
+    const q = query(postHomeCollection, where('faculty', '==', searchQuery));
+  
+    const querySnapshot = await getDocs(q);
+    const postHomeData = [];
+  
+    querySnapshot.forEach((doc) => {
+      const itemName = doc.data().name; // ประเภทสินค้า
+      if (itemName.includes(search)) { // ตรวจสอบว่าคำที่ค้นหาอยู่ในชื่อสินค้าหรือไม่
+        postHomeData.push({ id: doc.id, ...doc.data() });
+      }
+    });
+  
+    setFilteredData(postHomeData);
+  };
   return (
     <View>
       <Searchbar
@@ -34,15 +57,13 @@ const Search = () => {
         onChangeText={handleSearch}
         value={searchQuery}
       />
-     
-      {filteredData.map((item) => (
-        
-        <Text style={{left:20}} key={item.id}>{item.name}</Text>
-     
-      ))}
-     
+      <FlatList
+  data={filteredData}
+  renderItem={renderItem}
+  keyExtractor={(item) => item.id.toString()}
+/>
     </View>
   );
 };
 
-export default Search;
+export default Searchba;
--- screen/Home.js
+++ screen/Home.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Appbar, Searchbar } from 'react-native-paper';
-import { Text, StyleSheet, TouchableOpacity, View, Image, ScrollView, SafeAreaView, StatusBar } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity, View, Image, SafeAreaView, StatusBar, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; 
 import { onAuthStateChanged } from 'firebase/auth';
-import Search from '../components/Searchbar';
+import Searchba from '../components/Searchbar';
 import PostHome from '../components/PostHome';
 
 const MyComponent = () => {
@@ -18,56 +18,50 @@ const MyComponent = () => {
   };
 
   const handleCreatePostPress = () => {
-   
     navigation.navigate('Createpost');
-
   };
 
   return (
     <SafeAreaView style={styles.container}>
-        <Appbar.Header style={{ backgroundColor: '#FDF4E2'  , height: 30, top:-15}}>
-          <Image style={styles.logo} source={require('../assets/2.png')} />
-          <Appbar.Content title="Home" style={{ left: 65 }} />
-          <Appbar.Action
-            icon="bell"
-            style={{ marginLeft: 10 }}
-            onPress={() => navigation.navigate('NotiScreen')}
-          />
-        </Appbar.Header>
-        <ScrollView>
-        <Search style={styles.search} onSearchResults={handleSearchResults} />
-        <View>
-          <TouchableOpacity
-            style={{
-              borderRadius: 5,
-              borderWidth: 2,
-              backgroundColor: '#FDF4E2',
-              width: 90,
-              padding: 10,
-              marginTop: 25,
-              marginLeft: 290,
-            }}
-            onPress={handleCreatePostPress} // เมื่อปุ่มถูกกด
-          >
-            <Text style={{color:"#1C1441"}}>สร้างโพสต์</Text>
-          </TouchableOpacity>
-        </View>
-        {isSearching ? (
-        searchResults.map((post) => (
-          <View key={post.id} style={{ marginTop: 12 }}>
-            <PostHome post={post} />
-          </View>
-        ))
+      <Appbar.Header style={{ backgroundColor: '#FDF4E2'  , height: 30, top:-15}}>
+        <Image style={styles.logo} source={require('../assets/2.png')} />
+        <Appbar.Content title="Home" style={{ left: 65 }} />
+        <Appbar.Action
+          icon="bell"
+          style={{ marginLeft: 10 }}
+          onPress={() => navigation.navigate('NotiScreen')}
+        />
+      </Appbar.Header>
+      <Searchba style={styles.search} onSearchResults={handleSearchResults} />
+      <View>
+        <TouchableOpacity
+          style={{
+            borderRadius: 5,
+            borderWidth: 2,
+            backgroundColor: '#FDF4E2',
+            width: 90,
+            padding: 10,
+            marginTop: 25,
+            marginLeft: 290,
+          }}
+          onPress={handleCreatePostPress} // เมื่อปุ่มถูกกด
+        >
+          <Text style={{color:"#1C1441"}}>สร้างโพสต์</Text>
+        </TouchableOpacity>
+      </View>
+      {searchResults.length > 0 ? (
+        <FlatList
+          data={searchResults}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handlePostPress(item)}>
+              <PostHome item={item} />
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
       ) : (
-        // ถ้ายังไม่มีการค้นหา ให้แสดงโพสต์ทั้งหมด
-        <PostHome />
+        <PostHome/>
       )}
-        
-        <Text></Text>
-        <Text></Text>
-        <Text></Text>
-        <Text></Text>
-      </ScrollView>
     </SafeAreaView>
   );
 };

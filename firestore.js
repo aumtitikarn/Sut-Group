fcf320890d781e89cbe1df010683c8256d7f8133
aumtitikarn <aumzaa321@gmail.com>
2023-11-01 12:10:45 +0700

    .

--- firestore.js
+++ firestore.js
@@ -5,13 +5,14 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2AdVVRm3CHrAOmjQn9rO-mL6SzvgR8uE",
-  authDomain: "sut-group89.firebaseapp.com",
-  projectId: "sut-group89",
-  storageBucket: "sut-group89.appspot.com",
-  messagingSenderId: "870612525828",
-  appId: "1:870612525828:web:bd7d2200e24e6cb686e651",
-  measurementId: "G-CG5THM5RS0"
+  apiKey: "AIzaSyCmv-nA6FyGMuKq9jyucEIDNco_cm4WW1A",
+  authDomain: "sut-group123.firebaseapp.com",
+  databaseURL: "https://sut-group123-default-rtdb.firebaseio.com",
+  projectId: "sut-group123",
+  storageBucket: "sut-group123.appspot.com",
+  messagingSenderId: "879415393779",
+  appId: "1:879415393779:web:17c4bf6f0bc5ed312aa686",
+  measurementId: "G-8NVY4871KT"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig) ;

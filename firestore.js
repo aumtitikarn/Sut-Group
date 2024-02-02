73e0bd8a035a6f061fd54cc3741f1199653a55d6
aumtitikarn <aumzaa321@gmail.com>
2024-02-02 17:22:10 +0700

    .

--- firestore.js
+++ firestore.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBv6UbWpSjHTu4XhLAAgkfLJtq3fHpeN8",
-  authDomain: "project63-48da1.firebaseapp.com",
-  projectId: "project63-48da1",
-  storageBucket: "project63-48da1.appspot.com",
-  messagingSenderId: "981567715976",
-  appId: "1:981567715976:web:602a5e10f264d52f8fbd3d",
-  measurementId: "G-BC9623LV1P"
+  apiKey: "AIzaSyBnll_gpEJJYXM9SF6E1RgDwdUp1oO9M4s",
+  authDomain: "project63-33e66.firebaseapp.com",
+  projectId: "project63-33e66",
+  storageBucket: "project63-33e66.appspot.com",
+  messagingSenderId: "910132595498",
+  appId: "1:910132595498:web:2b5678949a580cdc46ac53",
+  measurementId: "G-JS1NCES124"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig) ;
